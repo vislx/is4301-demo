82ebcf9fced575ac0c1bd70629c6e458d0c426a3
victor <github@vislx.com>
2017-10-18 16:56:07 +0800

    market trends

--- src/app/db-market-trends/db-market-trends.component.ts
+++ src/app/db-market-trends/db-market-trends.component.ts
@@ -6,6 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./db-market-trends.component.css']
 })
 export class DbMarketTrendsComponent implements OnInit {
+  public chartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+    scales: {
+      xAxes: [{
+        display: false
+      }],
+      yAxes: [{
+        display: false,
+        ticks: {
+          suggestedMin: 0
+        }
+      }]
+    }
+  };
+  // public chartColors: [any] = [
+  //   { // grey
+  //     backgroundColor: 'rgba(156, 219, 243, 0.2)',
+  //     borderColor: 'rgba(156, 219, 243, 1)',
+  //     pointBackgroundColor: 'rgba(156, 219, 243, 1)',
+  //     pointBorderColor: '#fff',
+  //     pointHoverBackgroundColor: '#fff',
+  //     pointHoverBorderColor: 'rgba(156, 219, 243, 0.8)'
+  //   }];
+  public chartLegend = true;
+  public chartLabels: string[] = ['23 Jan', '26 Jan', '27 Jan', '28 Jan', '29 Jan', '30 Jan'];
+  public chartData: any[] = [
+    {
+      data: [65.00, 59.00, 55.00, 25.00, 51.00, 54.00],
+      label: 'Equity'
+    }, {
+      data: [55.00, 34.00, 81.00, 55.00, 29.00, 65.00],
+      label: 'Debt'
+    }
+  ];
 
   constructor() { }
 
