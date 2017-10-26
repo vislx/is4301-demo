612353afabc8242a3e0a614bc5789e1a7d16d652
victor <github@vislx.com>
2017-10-26 17:01:07 +0800

    switch to admin style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,13 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { NewsComponent } from './news/news.component';
-import { ChatbotComponent } from './chatbot/chatbot.component';
-import { DbIndicatorCardComponent } from './db-indicator-card/db-indicator-card.component';
-import { DbLineChartComponent } from './db-line-chart/db-line-chart.component';
+
 
 import { ChartsModule } from 'ng2-charts';
-import { DbMarketTrendsComponent } from './db-market-trends/db-market-trends.component';
-import { DbRecentActivitiesComponent } from './db-recent-activities/db-recent-activities.component';
-import { DbActivityComponent } from './db-activity/db-activity.component';
-import { ChatMessageComponent } from './chat-message/chat-message.component';
-import { NewsCardComponent } from './news-card/news-card.component';
-import { ChatInputboxComponent } from './chat-inputbox/chat-inputbox.component';
+
 
 const appRoutes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'news', component: NewsComponent },
-  { path: 'chat', component: ChatbotComponent },
   { path: '',
     redirectTo: '/dashboard',
     pathMatch: 'full'
@@ -34,17 +24,7 @@ const appRoutes: Routes = [
     AppComponent,
     NavComponent,
     ToolbarComponent,
-    DashboardComponent,
-    NewsComponent,
-    ChatbotComponent,
-    DbIndicatorCardComponent,
-    DbLineChartComponent,
-    DbMarketTrendsComponent,
-    DbRecentActivitiesComponent,
-    DbActivityComponent,
-    ChatMessageComponent,
-    NewsCardComponent,
-    ChatInputboxComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
